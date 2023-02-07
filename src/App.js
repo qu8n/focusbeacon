b04c2e197ed4ac1dfd73d027dc81961cb65f719a
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-02-07 01:11:36 -0500

    Experiment with fetching profile data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState, useEffect } from 'react';
+
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const myHeaders = new Headers();
+    myHeaders.append("X-API-KEY", process.env.REACT_APP_FOCUSMATE_API_KEY);
+
+    const requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    fetch("https://api.focusmate.com/v1/me", requestOptions)
+      .then(response => response.text())
+      .then(result => setData(result))
+      .catch(error => console.log('error', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data ? <p>{data}</p> : <p>Loading...</p>}
     </div>
   );
 }
 
 export default App;
+
