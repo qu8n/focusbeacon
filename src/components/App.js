e7d7c52cc689b7067fb0e224cd2881b143e0455b
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-02-08 19:44:38 -0500

    Replace fetched profile data with a barebone display

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <>
       <Profile/>
+      <br/>
       <Sessions/>
     </>
   )
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,10 +3,19 @@ import useFetchData from '../hooks/useFetchData';
 
 export default function Profile() {
     const [loading, data] = useFetchData('me', 'profile');
+    const {name, totalSessionCount, timeZone, photoUrl} = data;
+
+    const profileCard = 
+        <>
+            <img src={photoUrl} alt='profile' style={{'border-radius':'50%', 'height':'100px'}}/>
+            <p>{name}</p>
+            <p>{totalSessionCount}</p>
+            <p>{timeZone}</p>
+        </>
 
     return (
         <div>
-            {loading ? 'Loading...' : JSON.stringify(data)}
+            {loading ? 'Loading...' : profileCard}
         </div>
     );
 }
