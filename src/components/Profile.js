21c85190326d9e9bfe108903b0c0a4951e08bcaf
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-02-08 20:51:14 -0500

    Style the profile card

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import useFetchData from '../hooks/useFetchData';
+import '../styles/profile.css'
 
 export default function Profile() {
     const [loading, data] = useFetchData('me', 'profile');
     const {name, totalSessionCount, timeZone, photoUrl} = data;
 
     const profileCard = 
-        <>
-            <img src={photoUrl} alt='profile' style={{'border-radius':'50%', 'height':'100px'}}/>
-            <p>{name}</p>
-            <p>{totalSessionCount}</p>
-            <p>{timeZone}</p>
-        </>
+    <>
+        <div className='profile-card'>
+            <div className='box'>
+                <img className='image' src={ photoUrl } alt="profile-img"/>
+                <h1 className='name'>{ name }</h1>
+                <p className='description'>{ totalSessionCount } sessions</p>
+                <p className='description'>{ timeZone }</p>
+            </div>
+        </div>
+    </>
 
     return (
         <div>
