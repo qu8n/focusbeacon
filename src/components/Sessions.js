adf3d4d6cb5eb31b49fd6e8f53016b38f682be7d
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-02-09 11:20:39 -0500

    Clean up session duration count table

--- src/components/Sessions.js
+++ src/components/Sessions.js
@@ -2,15 +2,14 @@ import React from 'react';
 import useFetchData from '../hooks/useFetchData';
 
 function displaySessions(data) {
-    let table = {};
-    for (let index in data) {
-        let duration = data[index].duration;
-        if (!(duration in table)) {
-            table[duration] = 0
-        };
-        table[duration] += 1;
+    let table = {
+        '25 minutes': 0,
+        '50 minutes': 0,
+        '75 minutes': 0
     };
-
+    for (let index in data) {
+        table[`${data[index].duration / 60000} minutes`] += 1;
+    };
     return (
         <table>    
             {Object.entries(table).map( ([key, value]) => ( 
@@ -18,11 +17,11 @@ function displaySessions(data) {
             ))}
         </table>
     )
-}
+};
 
 export default function Sessions() {
     // TODO make dates dynamic
-    const [loading, data] = useFetchData('sessions?start=2022-12-28T12:00:00Z&end=2022-12-29T12:00:00Z', 'sessions');
+    const [loading, data] = useFetchData('sessions?start=2022-12-20T12:00:00Z&end=2022-12-29T12:00:00Z', 'sessions');
 
     return (
         <>
