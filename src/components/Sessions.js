f0290e4fea0497f4c1408ca2ac91869e346f6644
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-02-09 14:28:06 -0500

    Display session count by duration table with AG Grid

--- src/components/Sessions.js
+++ src/components/Sessions.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useFetchData from '../hooks/useFetchData';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 function displaySessions(data) {
     let table = {
@@ -10,22 +13,40 @@ function displaySessions(data) {
     for (let index in data) {
         table[`${data[index].duration / 60000} minutes`] += 1;
     };
-    return (
-        <table>    
-            {Object.entries(table).map( ([key, value]) => ( 
-                <tr key={key}><td>{key}</td><td>{value}</td></tr>
-            ))}
-        </table>
-    )
+
+    const agTableData = [];
+
+    for (const [key, value] of Object.entries(table)) {
+        agTableData.push({
+            duration: key,
+            sessions: value
+        });
+    }
+    return agTableData;
 };
 
 export default function Sessions() {
     // TODO make dates dynamic
     const [loading, data] = useFetchData('sessions?start=2022-12-20T12:00:00Z&end=2022-12-29T12:00:00Z', 'sessions');
+    const [rowData, setRowData] = useState();
+    const [columnDefs] = useState([
+        { field: 'duration' },
+        { field: 'sessions' },
+    ]);
 
-    return (
-        <>
-            {loading ? 'Loading...' : displaySessions(data)}
-        </>
-    );
+    useEffect(() => {
+        if (!loading && data) {
+            setRowData(displaySessions(data))
+        }
+    }, [loading, data]);
+
+    if (loading) {
+        return 'Loading...'
+    } else {
+        return (
+            <div className="ag-theme-alpine" style={{ height: 173, width: 402 }}>
+              <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+            </div>
+        );
+    }
 }
