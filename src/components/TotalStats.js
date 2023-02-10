09d8f5fb3ed064715fe7a48df6de59c78e7972fa
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-02-10 18:29:40 -0500

    Add total metric table

--- src/components/TotalStats.js
+++ src/components/TotalStats.js
@@ -1,14 +1,41 @@
+import React, { useEffect, useState } from 'react';
 import useFetchData from '../hooks/useFetchData';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 export default function TotalStats() {
     const [loading, data] = useFetchData('sessions?start=2022-01-01T12:00:00Z&end=2023-01-01T12:00:00Z', 'sessions');
-    return loading ? 'Loading...' : `~${process(data)}`
+    const [rowData, setRowData] = useState();
+    const [columnDefs] = useState([
+        { field: 'metric', width: 250, suppressMovable:true },
+        { field: 'value', width: 140, suppressMovable:true, type: 'numericColumn' }
+    ]);
+
+    useEffect(() => {
+        if (!loading && data) {
+            setRowData(process(data))
+        }
+    }, [loading, data]);
+
+    if (loading) {
+        return 'Loading...'
+    } else {
+        return (
+            <div className="ag-theme-alpine" style={{ height: 270, width: 400 }}>
+                <AgGridReact 
+                    rowData={rowData} 
+                    columnDefs={columnDefs} 
+                ></AgGridReact>
+            </div>
+        );
+    };
 };
 
 function process(data) {
-    let totalHours = 0; // metric
-    const totalSessionsBooked = data.length; // metric
-    let totalSessionsCompleted = 0; // metric
+    const totalSessionsBooked = data.length;
+    let totalSessionsCompleted = 0;
+    let totalHours = 0;
     let uniquePartners = new Set();
     let currentPartner = '';
     
@@ -24,8 +51,23 @@ function process(data) {
         }
     };
 
-    const totalPartners = uniquePartners.size; // metric
+    let table = {
+        'Total Sessions Booked': totalSessionsBooked,
+        'Total Sessions Completed': totalSessionsCompleted,
+        '% Booked Sessions Completed': Math.round(totalSessionsCompleted / totalSessionsBooked * 100) + '%',
+        'Total Hours of Sessions': totalHours,
+        'Total Partners': uniquePartners.size
+    };
 
-    return null // placeholder
+    const agTableData = [];
+
+    for (const [key, value] of Object.entries(table)) {
+        agTableData.push({
+            metric: key,
+            value: value.toLocaleString(),
+        });
+    };
+
+    return agTableData;
 };
 
