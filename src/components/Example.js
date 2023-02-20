305a1ac22331d4295c0601993a0355bd5989e189
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-02-20 13:58:35 -0500

    Update KPI card UI

--- src/components/Example.js
+++ src/components/Example.js
@@ -9,43 +9,38 @@ import {
 } from '@tremor/react';
 
 import {
-    CashIcon,
-    TicketIcon,
-    UserGroupIcon,
+    ClockIcon,
+    VideoCameraIcon,
+    UsersIcon,
+    ChartPieIcon,
 } from '@heroicons/react/solid';
 
 import React from 'react';
 
 const categories = [
     {
-        title: 'Sales',
-        metric: '$ 23,456,456',
-        icon: TicketIcon,
+        title: 'Total Sessions',
+        metric: '672',
+        icon: VideoCameraIcon,
         color: 'indigo',
     },
     {
-        title: 'Profit',
-        metric: '$ 13,123',
-        icon: CashIcon,
-        color: 'fuchsia',
-    },
-    {
-        title: 'Customers',
-        metric: '456',
-        icon: UserGroupIcon,
+        title: 'Total Hours',
+        metric: '546',
+        icon: ClockIcon,
         color: 'amber',
     },
     {
-        title: 'Customers',
-        metric: '456',
-        icon: UserGroupIcon,
-        color: 'amber',
+        title: 'Total Unique Partners',
+        metric: '611',
+        icon: UsersIcon,
+        color: 'orange',
     },
 ];
 
 export default function Example() {
     return (
-        <ColGrid numColsSm={ 2 } numColsLg={ 4 } gapX="gap-x-6" gapY="gap-y-6">
+        <ColGrid numColsSm={ 3 } numColsLg={ 3 } gapX="gap-x-6" gapY="gap-y-6">
             { categories.map((item) => (
                 <Card key={ item.title } decoration="top" decorationColor={ item.color }>
                     <Flex justifyContent="justify-start" spaceX="space-x-4">
