0b787ac0fd1c023c337954fd1b8397f5c617261e
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-02-20 15:25:20 -0500

    Simplify ProfileCard component

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -3,18 +3,15 @@ import '../styles/ProfileCard.css'
 
 export default function ProfileCard(props) {
     const [loading, data] = props.data;
-    const {name, timeZone, photoUrl} = data;
+    const {name, photoUrl} = data;
 
     const profileCard = 
-    <>
         <div className='profile-card'>
-            <div className='box'>
+            <div>
                 <img className='image' src={ photoUrl } alt="profile-img"/>
                 <h1 className='name'>{ name }</h1>
-                <p className='description'>{ timeZone }</p>
             </div>
         </div>
-    </>
 
     return (
         <div>
