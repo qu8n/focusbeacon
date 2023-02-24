1d796ea03464b9b491c449514156fcd5fe62b53e
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-02-24 07:08:07 -0500

    Save unfinished work with fetching partner data

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -4,6 +4,7 @@ export default function useFetchData() {
     const [loading, setLoading] = useState(true);
     const [profileData, setProfileData] = useState([]);
     const [sessionsData, setSessionsData] = useState([]);
+    const [partnerData, setPartnerData] = useState([]);
 
     function requestOptions() {
         const myHeaders = new Headers();
@@ -26,31 +27,53 @@ export default function useFetchData() {
             
             // Multiple requests are needed because the API only returns one year of data per request
             const currentYear = new Date().getFullYear();
-            let sessionData = [];
+            let rawSessionData = [];
             for (let year = currentYear; year >= 2016; year--) {
                 try {
                     const response = await fetch(`https://api.focusmate.com/v1/sessions?start=${year}-01-01T12:00:00Z&end=${year}-12-31T12:00:00Z`, requestOptions());
                     const result = await response.json();
-                    sessionData = [...sessionData, ...result.sessions];
+                    rawSessionData = [...rawSessionData, ...result.sessions];
                 } catch (error) {
                    console.log(`Error fetching data for year ${year}: ${error.message}`);
                 };
             };
 
-            const filteredSessionData = sessionData.filter(session =>
-                session.users[0].completed === true
+            // Exclude incomplete sessions because they're not "counted"
+            const filteredSessionData = rawSessionData.filter(
+                session => session.users[0].completed === true
             );
 
             setSessionsData(filteredSessionData);
+        };
+
+        // const fetchPartnerData = async () => {
+        //     const filteredSessionData = sessionsData.filter(
+        //         session => session.users.length === 2
+        //     );
+
+        //     let rawPartnerData = [];
+        //     for (let session of filteredSessionData) {
+        //         try {
+        //             const response = await fetch(`https://api.focusmate.com/v1/${session.users[1].userId}`, requestOptions());
+        //             const result = await response.json();
+        //             rawPartnerData = [...rawPartnerData, ...result.sessions];
+        //         } catch (error) {
+        //             console.log(`Error fetching data for session ${session.sessionId}: ${error.message}`);
+        //         };
+        //     };
+
+        //     setPartnerData(rawPartnerData);
+        // };
+
+        const fetchAllData = async () => {
+            await fetchProfileAndSessionsData();
+            // await fetchPartnerData();
+            
             setLoading(false);
         };
 
-        const fetchPartnerProfileData = async () => {
-            const profileAndSessionsResponse = await fetchProfileAndSessionsData();
-        };
-
-        fetchPartnerProfileData();
+        fetchAllData();
     }, []);
-
+    
     return [loading, profileData, sessionsData];
 }
