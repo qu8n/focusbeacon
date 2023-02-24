ccd3433a750bd7d9ef1e89e34ba15476dea1bd49
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-02-23 21:03:38 -0500

    Refactor requestOptions to be DRYer and start work on partner profile fetch

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -1,22 +1,23 @@
 import { useState, useEffect } from 'react';
 
 export default function useFetchData() {
-    const [profileData, setProfileData] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [profileData, setProfileData] = useState([]);
     const [sessionsData, setSessionsData] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const myHeaders = new Headers();
-            myHeaders.append("X-API-KEY", process.env.REACT_APP_FOCUSMATE_API_KEY);
-
-            const requestOptions = {
+    function requestOptions() {
+        const myHeaders = new Headers();
+        myHeaders.append("X-API-KEY", process.env.REACT_APP_FOCUSMATE_API_KEY);
+        return {
             method: 'GET',
             headers: myHeaders,
             redirect: 'follow'
-            };
+        };
+    }
 
-            fetch(`https://api.focusmate.com/v1/me`, requestOptions)
+    useEffect(() => {
+        const fetchProfileAndSessionsData = async () => {
+            fetch(`https://api.focusmate.com/v1/me`, requestOptions())
                 .then(response => response.json())
                 .then((result) => {
                     setProfileData(result.user);
@@ -28,7 +29,7 @@ export default function useFetchData() {
             let data = [];
             for (let year = currentYear; year >= 2016; year--) {
                 try {
-                    const response = await fetch(`https://api.focusmate.com/v1/sessions?start=${year}-01-01T12:00:00Z&end=${year}-12-31T12:00:00Z`, requestOptions);
+                    const response = await fetch(`https://api.focusmate.com/v1/sessions?start=${year}-01-01T12:00:00Z&end=${year}-12-31T12:00:00Z`, requestOptions());
                     const result = await response.json();
                     data = [...data, ...result.sessions];
                 } catch (error) {
@@ -40,7 +41,11 @@ export default function useFetchData() {
             setLoading(false);
         };
 
-        fetchData();
+        const fetchPartnerProfileData = async () => {
+            const profileAndSessionsResponse = await fetchProfileAndSessionsData();
+        };
+
+        fetchPartnerProfileData();
     }, []);
 
     return [loading, profileData, sessionsData];
