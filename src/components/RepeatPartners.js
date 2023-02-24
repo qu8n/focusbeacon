e959f7c1df5f25ecbcab20c064cf0cdf2f28ff33
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-02-24 16:47:41 -0500

    Add a tooltip to RepeatPartners

--- src/components/RepeatPartners.js
+++ src/components/RepeatPartners.js
@@ -7,9 +7,10 @@ import {
     TableCell,
     Card,
     Title,
-    Text,
-    Footer,
+    Icon,
+    Flex,
   } from "@tremor/react";
+import { ChatIcon } from '@heroicons/react/solid';
 
 export default function RepeatPartners({data}) {
     const [loading, repeatPartnersArr] = data;
@@ -19,24 +20,33 @@ export default function RepeatPartners({data}) {
     } else {
         return (
             <Card>
-            <Title>Repeat Session Partners</Title>
-            <Table marginTop="mt-5">
-                <TableHead>
-                    <TableRow>
-                        <TableHeaderCell textAlignment="text-left">Shared Sessions</TableHeaderCell>
-                        <TableHeaderCell textAlignment="text-right">Number of Partners</TableHeaderCell>
-                    </TableRow>
-                </TableHead>
+                <Flex alignItems="align-top">
+                    <Title>Repeat Session Partners</Title>
+                    <Icon
+                        icon={ChatIcon}
+                        variant="simple"
+                        tooltip="More details available at focusmate.com/people, where you can sort by 'Sessions together'.
+                            Note that deleted accounts are counted as one-session partners."
+                        color="gray"
+                    />
+                </Flex>
+                <Table marginTop="mt-5">
+                    <TableHead>
+                        <TableRow>
+                            <TableHeaderCell textAlignment="text-left">Shared Sessions</TableHeaderCell>
+                            <TableHeaderCell textAlignment="text-right">Number of Partners</TableHeaderCell>
+                        </TableRow>
+                    </TableHead>
 
-                <TableBody>
-                    { repeatPartnersArr.map((sharedSession) => (
-                    <TableRow key={ sharedSession.sharedSessions }>
-                        <TableCell textAlignment="">{ sharedSession.sharedSessions.toLocaleString() } { sharedSession.sharedSessions > 1 ? 'sessions' : 'session' }</TableCell>
-                        <TableCell textAlignment="text-right">{ sharedSession.partners.toLocaleString() }</TableCell>
-                    </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+                    <TableBody>
+                        { repeatPartnersArr.map((sharedSession) => (
+                        <TableRow key={ sharedSession.sharedSessions }>
+                            <TableCell textAlignment="">{ sharedSession.sharedSessions.toLocaleString() } { sharedSession.sharedSessions > 1 ? 'sessions' : 'session' }</TableCell>
+                            <TableCell textAlignment="text-right">{ sharedSession.partners.toLocaleString() }</TableCell>
+                        </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
             </Card>
         );
     }
