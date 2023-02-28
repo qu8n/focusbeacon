808f074bba8f8f01c037a64d1c14a9269b6db5df
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-02-28 08:32:17 -0500

    Update the navigation bar logo to FocusmateData

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -28,13 +28,8 @@ export default function NavBar() {
                 </div>
                 <div className="flex flex-shrink-0 items-center">
                   <img
-                    className="block h-8 w-auto lg:hidden"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                    alt="Your Company"
-                  />
-                  <img
-                    className="hidden h-8 w-auto lg:block"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    className="h-8 w-auto"
+                    src="/navBarLogo.png"
                     alt="Your Company"
                   />
                 </div>
