24f00e739bb7e3668d1d3b68efedcd7d2e1a4702
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-02-28 09:17:47 -0500

    Update logos and texts from FocusStats to FocusmateData

--- src/components/Logo.js
+++
@@ -1,7 +0,0 @@
-import "../styles/Logo.css"
-
-export default function Logo() {
-    return (
-        <img className="logo" src={"https://i.imgur.com/EAjvavl.png"} alt="FocusMate logo"/>
-    );
-};
