9f72957f09798c48d75874f03734f0471dea313d
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-03-28 15:37:45 -0400

    WIP migration to Next: move the previous index.js content to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "../styles/globals.css";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 App.propTypes = {
   Component: PropTypes.elementType.isRequired,
   pageProps: PropTypes.object.isRequired
 };
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
