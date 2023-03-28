dd43a554190c47f3063a90e53e1f235d4be3c879
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-03-28 15:54:35 -0400

    WIP migration to Next: reconfig Tailwind and move some tags from _doc to _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../styles/globals.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import Head from "next/head";
 
 App.propTypes = {
   Component: PropTypes.elementType.isRequired,
@@ -12,8 +13,41 @@ const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* https://realfavicongenerator.net/ */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#e9520e" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta
+          name="description"
+          content="Focusmate.com analytics for building better habits and crushing your goals"
+        />
+        {/* manifest.json provides metadata used when your web app is installed on a user's mobile device or desktop */}
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </>
   );
 }
