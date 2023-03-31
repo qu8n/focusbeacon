a7ddb6b37a575a4a3c12083bc37b8228501f9352
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-03-31 13:53:43 -0400

    Redesign landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,21 @@ import SignInButton from "../components/SignInButton";
 
 export default function Landing() {
   return (
-    <>
-      <p className="p-10 mt-32 text-center text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-slate-600 to-slate-400">
-        Unlock enhanced insights into your Focusmate sessions
-      </p>
-      <div className="flex justify-center">
-        <SignInButton />
+    <div className="flex p-10 h-5/6 bg-no-repeat bg-center bg-[url('../../public/pie-bg.png')]">
+      <div className="m-auto">
+        <p className="text-center text-5xl font-bold text-slate-500">
+          Unlock your Focusmate stats
+        </p>
+        <p className="text-center text-3xl font-normal text-slate-400">
+          Build better habits & crush your goals
+        </p>
+        <div className="mt-5 flex justify-center">
+          <SignInButton />
+        </div>
+        <p className="mt-2 text-center text-slate-400 text-sm">
+          Tip: Sign into Focusmate from your device first
+        </p>
       </div>
-      <p className="mt-2 text-center text-slate-400 text-sm">
-        (Please sign in on Focusmate first)
-      </p>
-    </>
+    </div>
   );
 }
