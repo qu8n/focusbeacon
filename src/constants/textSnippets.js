86640dea380699d0a9d5066a11937d5dba876a31
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-04-25 13:08:46 -0400

    Render dashboard using data fetched with access token

---
+++ src/constants/textSnippets.js
@@ -0,0 +1,5 @@
+export const weeklyChartTooltip =
+  "Each x-axis marker represents a week, which begins on Sunday based on the Gregorian calendar";
+
+export const monthlyChartTooltip =
+  "Each x-axis marker represents a month and its respective year";
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,6 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { useQuery } from "react-query";
+import LoaderSpinner from "../components/LoaderSpinner";
+import processData from "../utils/processData";
+import "@tremor/react/dist/esm/tremor.css";
+import { ColGrid } from "@tremor/react";
+import SessionsByDuration from "../components/SessionsByDuration";
+import LifetimeMetrics from "../components/LifetimeMetrics";
+import Milestones from "../components/Milestones";
+import RepeatPartners from "../components/RepeatPartners";
+import TimeSeriesChart from "../components/TimeSeriesChart";
+import Footer from "../components/Footer";
+import {
+  monthlyChartTooltip,
+  weeklyChartTooltip
+} from "../constants/textSnippets";
 
 export default function Dashboard() {
   const { isLoading, isError, data, error } = useQuery({
@@ -13,18 +27,100 @@ export default function Dashboard() {
   });
 
   if (isLoading) {
-    return <p>Loading...</p>;
+    return <LoaderSpinner />;
   }
 
   if (isError) {
+    // TODO: add link for user to report error
     return <p>Error: {error.message}</p>;
   }
 
-  console.log("data: ", data);
+  const { profileData, sessionsData } = data;
+
+  const [
+    totalSessions,
+    totalHours,
+    totalPartners,
+    firstSessionDate,
+    maxHoursADay,
+    sessionsByDurationArr,
+    milestonesArr,
+    repeatPartnersArr,
+    lTMSessionsArr,
+    lTMHoursArr,
+    updateTime,
+    lTWSessionsArr,
+    lTWHoursArr
+  ] = processData(sessionsData);
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <>
+      <div className="m-7">
+        <LifetimeMetrics
+          data={[
+            totalSessions,
+            totalHours,
+            totalPartners,
+            firstSessionDate,
+            maxHoursADay
+          ]}
+        />
+      </div>
+
+      <div className="m-7">
+        <ColGrid numColsLg={3} gapX="gap-x-6" gapY="gap-y-6">
+          <SessionsByDuration data={[sessionsByDurationArr, totalSessions]} />
+          <Milestones data={milestonesArr} />
+          <RepeatPartners data={repeatPartnersArr} />
+        </ColGrid>
+      </div>
+
+      <div className="m-7">
+        <ColGrid numColsLg={2} gapX="gap-x-6" gapY="gap-y-6">
+          <TimeSeriesChart
+            chartType="bar"
+            title="Sessions by Week"
+            data={lTWSessionsArr}
+            dataKey="Week of"
+            categories={["Number of Sessions"]}
+            tooltip={weeklyChartTooltip}
+          />
+          <TimeSeriesChart
+            chartType="area"
+            title="Hours of Sessions by Week"
+            data={lTWHoursArr}
+            dataKey="Week of"
+            categories={["Hours of Sessions"]}
+            tooltip={weeklyChartTooltip}
+          />
+        </ColGrid>
+      </div>
+
+      <div className="m-7">
+        <ColGrid numColsLg={2} gapX="gap-x-6" gapY="gap-y-6">
+          <TimeSeriesChart
+            chartType="bar"
+            title="Sessions by Month"
+            data={lTMSessionsArr}
+            dataKey="Month"
+            categories={["Number of Sessions"]}
+            tooltip={monthlyChartTooltip}
+          />
+          <TimeSeriesChart
+            chartType="area"
+            title="Hours of Sessions by Month"
+            data={lTMHoursArr}
+            dataKey="Month"
+            categories={["Hours of Sessions"]}
+            tooltip={monthlyChartTooltip}
+          />
+        </ColGrid>
+      </div>
+
+      <div className="m-7">
+        <Footer data={updateTime} />
+      </div>
+      <br />
+    </>
   );
 }
