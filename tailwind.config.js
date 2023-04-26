4af21e5c95fae499814b7ba541124945e5e1c1fe
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-04-26 18:52:45 -0400

    Add gradient animation to the bg of welcome page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,29 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+    extend: {
+      animation: {
+        blob: "blob 7s infinite"
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)"
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)"
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)"
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)"
+          }
+        }
+      }
+    }
+  },
+  variants: {
     extend: {}
   },
   plugins: []
