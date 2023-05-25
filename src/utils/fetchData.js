1a9a9106ce448e46df8360aa6f3a0088998b48de
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-05-25 19:57:19 -0400

    Fetch sessions data by half year increment

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -14,14 +14,22 @@ export async function fetchSessionsData(headers, memberSince) {
 
   // Limit requests to one year of data at a time following API's restriction.
   for (let year = currentYear; year >= firstYear; year--) {
-    // End the time range at 12:00:00 because anything above results in an error
-    // 'Date range must be smaller than one year'.
-    const response = await fetch(
-      `https://api.focusmate.com/v1/sessions?start=${year}-01-01T12:00:00Z&end=${year}-12-31T12:00:00Z`,
+    // Splitting into two requests per year to avoid the API's error:
+    // 'Date range must be smaller than one year' for select accounts when using the full year range
+    // ?start=${year}-01-01T00:00:00Z&end=${year}-12-31T23:59:59Z`
+    const response1 = await fetch(
+      `https://api.focusmate.com/v1/sessions?start=${year}-01-01T00:00:00Z&end=${year}-06-30T23:59:59Z`,
       requestOptions(headers)
     );
-    const result = await response.json();
-    sessionsData = [...sessionsData, ...result.sessions];
+    const result1 = await response1.json();
+    sessionsData = [...sessionsData, ...result1.sessions];
+
+    const response2 = await fetch(
+      `https://api.focusmate.com/v1/sessions?start=${year}-07-01T00:00:00Z&end=${year}-12-31T23:59:59Z`,
+      requestOptions(headers)
+    );
+    const result2 = await response2.json();
+    sessionsData = [...sessionsData, ...result2.sessions];
   }
 
   return sessionsData;
