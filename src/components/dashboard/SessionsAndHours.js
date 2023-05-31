382a87f699a1b8b4cc31473a5a710e544382da86
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-05-31 07:12:04 -0400

    Reformat the hours chart to avoid the 0-line issue

--- src/components/dashboard/SessionsAndHours.js
+++ src/components/dashboard/SessionsAndHours.js
@@ -1,17 +1,13 @@
-/* eslint-disable react/prop-types */
-import { BarChart, Card, Title, Text, AreaChart } from "@tremor/react";
+import { BarChart, Card, Title, Text } from "@tremor/react";
 import React from "react";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
-// SessionsAndHours.propTypes = {
-//   data: PropTypes.shape({
-//     sessions: PropTypes.arrayOf(PropTypes.object).isRequired,
-//     hours: PropTypes.arrayOf(PropTypes.object).isRequired
-//   }).isRequired,
-//   intervalInTitle: PropTypes.string.isRequired,
-//   chartIndex: PropTypes.string.isRequired,
-//   xAxisLabel: PropTypes.string.isRequired
-// };
+SessionsAndHours.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.object)).isRequired,
+  intervalInTitle: PropTypes.string.isRequired,
+  chartIndex: PropTypes.string.isRequired,
+  xAxisLabel: PropTypes.string.isRequired
+};
 
 export function SessionsAndHours({
   data: [sessions, hours],
@@ -37,16 +33,19 @@ export function SessionsAndHours({
 
       <Card>
         <Title>Hours of sessions by {intervalInTitle}</Title>
-        <AreaChart
+        <BarChart
           data={hours}
           index={chartIndex}
           categories={["25 minutes", "50 minutes", "75 minutes"]}
           colors={["blue", "orange", "yellow"]}
           yAxisWidth={32}
           stack={true}
-          valueFormatter={(value) => Math.round(value * 100) / 100}
+          valueFormatter={(number) => {
+            const hours = Math.floor(number);
+            const minutes = Math.round((number - hours) * 60);
+            return `${hours}h${minutes > 0 ? ` ${minutes}m` : ""}`;
+          }}
           allowDecimals={false}
-          showGradient={false}
         />
         <Text className="text-center">{xAxisLabel}</Text>
       </Card>
