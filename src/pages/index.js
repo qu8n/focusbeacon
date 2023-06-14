b2ca1cfbcb4e70567ab0833ea44a47e31c313241
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-06-14 02:56:55 -0400

    Fetch total users on landing page from server side

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { InformationCircleIcon } from "@heroicons/react/outline";
 import { VideoCameraIcon } from "@heroicons/react/solid";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { getOAuthURL } from "../utils/getOAuthURL";
 import FadeIn from "react-fade-in";
-import { ThreeDots } from "react-loader-spinner";
+import PropTypes from "prop-types";
 
-export default function Home() {
+Home.propTypes = {
+  totalUsers: PropTypes.number
+};
+
+export default function Home({ totalUsers }) {
   const router = useRouter();
-  const [totalUsers, setTotalUsers] = useState("__");
 
   useEffect(() => {
     async function checkSignedInStatus() {
@@ -20,82 +23,74 @@ export default function Home() {
       }
     }
     checkSignedInStatus();
-
-    async function getStats() {
-      const response = await fetch("/api/stats");
-      const data = await response.json();
-      setTotalUsers(data.totalUsers);
-    }
-    if (process.env.NODE_ENV === "production") {
-      getStats();
-    }
   }, []);
 
   const oauthURL = getOAuthURL();
 
   return (
-    <div className="flex items-center justify-center h-[calc(100vh-150px)] px-10">
-      <div className="relative w-full max-w-xl">
-        <div className="relative">
-          <p className="pb-2 -mt-12 text-4xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-br from-slate-700 to-slate-500">
-            Unlock your Focusmate metrics.
-          </p>
-          {process.env.NODE_ENV === "production" &&
-            (totalUsers > 0 ? (
-              <FadeIn transitionDuration={1000}>
-                <p className="text-lg font-normal text-center text-slate-700">
-                  Join{" "}
-                  <span className="underline decoration-orange-400 decoration-wavy">
-                    {totalUsers} other Focusmate users
-                  </span>{" "}
-                  and get access to your milestones, session trends, hours of
-                  session, and more.
-                </p>
-              </FadeIn>
-            ) : (
-              <div className="flex justify-center">
-                <div>
-                  <ThreeDots
-                    visible={true}
-                    height="50"
-                    width="50"
-                    radius="9"
-                    ariaLabel="three-dots-loading"
-                    wrapperStyle={{}}
-                    wrapperClass=""
-                    color="#b0bec5"
-                  />
-                </div>
-              </div>
-            ))}
-          <div className="flex justify-center mt-10 text-slate-500">
-            <InformationCircleIcon
-              className="w-4 h-4 mr-1"
-              aria-hidden="true"
-            />
-            <span className="text-xs w-72">
-              Log into Focusmate first before logging in here
-            </span>
-          </div>
-          <div className="flex justify-center">
-            <Link
-              href={oauthURL}
-              className="flex items-center justify-center h-16 py-3 mt-2 mb-2 mr-2 text-lg font-medium text-center text-white rounded-lg bg-gradient-to-r from-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 w-80"
-            >
-              <VideoCameraIcon className="w-6 h-6 mr-3" aria-hidden="true" />
-              Log in with Focusmate
-            </Link>
-          </div>
-          <div className="flex justify-center mt-4 text-lg font-semibold text-slate-700 hover:text-slate-500">
-            <Link href="/dashboard/demo">
-              View demo
-              <span aria-hidden="true" className="ml-1">
-                →
+    <FadeIn transitionDuration={500}>
+      <div className="flex items-center justify-center h-[calc(100vh-150px)] px-10">
+        <div className="relative w-full max-w-xl">
+          <div className="relative">
+            <p className="pb-2 -mt-12 text-4xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-br from-slate-700 to-slate-500">
+              Unlock your Focusmate metrics.
+            </p>
+            {process.env.NODE_ENV === "production" && (
+              <p className="text-lg font-normal text-center text-slate-700">
+                Join{" "}
+                <span className="underline decoration-orange-400 decoration-wavy">
+                  {totalUsers} other Focusmate users
+                </span>{" "}
+                and get access to your milestones, session trends, hours of
+                session, and more.
+              </p>
+            )}
+            <div className="flex justify-center mt-10 text-slate-500">
+              <InformationCircleIcon
+                className="w-4 h-4 mr-1"
+                aria-hidden="true"
+              />
+              <span className="text-xs w-72">
+                Log into Focusmate first before logging in here
               </span>
-            </Link>
+            </div>
+            <div className="flex justify-center">
+              <Link
+                href={oauthURL}
+                className="flex items-center justify-center h-16 py-3 mt-2 mb-2 mr-2 text-lg font-medium text-center text-white rounded-lg bg-gradient-to-r from-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 w-80"
+              >
+                <VideoCameraIcon className="w-6 h-6 mr-3" aria-hidden="true" />
+                Log in with Focusmate
+              </Link>
+            </div>
+            <div className="flex justify-center mt-4 text-lg font-semibold text-slate-700 hover:text-slate-500">
+              <Link href="/dashboard/demo">
+                View demo
+                <span aria-hidden="true" className="ml-1">
+                  →
+                </span>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </FadeIn>
   );
+}
+
+export async function getServerSideProps() {
+  const response = await fetch(
+    `${
+      process.env.NODE_ENV === "development"
+        ? process.env.NEXT_PUBLIC_DEV_URL
+        : process.env.NEXT_PUBLIC_PROD_URL
+    }/api/stats`
+  );
+  const data = await response.json();
+  const totalUsers = data.totalUsers;
+  return {
+    props: {
+      totalUsers
+    }
+  };
 }
