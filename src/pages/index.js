1e3193d942bb74258f9c16a663400f4afaf518f7
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-08-17 06:03:45 -0400

    Give screenshot a fade effect and get total user stats on client side

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { InformationCircleIcon } from "@heroicons/react/outline";
 import { VideoCameraIcon } from "@heroicons/react/solid";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { getOAuthURL } from "../utils/getOAuthURL";
 import FadeIn from "react-fade-in";
-import PropTypes from "prop-types";
 import Image from "next/image";
 
-Home.propTypes = {
-  totalUsers: PropTypes.number
-};
-
-export default function Home({ totalUsers }) {
+export default function Home() {
   const router = useRouter();
+  const [totalUsers, setTotalUsers] = useState(null);
 
   useEffect(() => {
     async function checkSignedInStatus() {
@@ -24,29 +20,28 @@ export default function Home({ totalUsers }) {
       }
     }
     checkSignedInStatus();
+
+    async function getStats() {
+      const response = await fetch("/api/stats");
+      const data = await response.json();
+      setTimeout(() => {
+        setTotalUsers(data.totalUsers);
+      }, 1000);
+    }
+    getStats();
   }, []);
 
   const oauthURL = getOAuthURL();
 
   return (
-    <FadeIn transitionDuration={500}>
-      <div className="flex items-center justify-center px-10 mt-44">
+    <>
+      <div className="flex items-center justify-center px-10 mt-32">
         <div className="relative w-full max-w-xl">
           <div className="relative">
             <p className="pb-2 -mt-12 text-4xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-br from-slate-700 to-slate-500">
-              Unlock your Focusmate metrics.
+              Unlock your Focusmate metrics
             </p>
-            {totalUsers && (
-              <p className="text-xl font-normal text-center text-slate-700">
-                Join{" "}
-                <span className="underline decoration-orange-400 decoration-wavy">
-                  {totalUsers} Focusmate users
-                </span>{" "}
-                and get access to your milestones, session trends, hours of
-                session, and more.
-              </p>
-            )}
-            <div className="flex justify-center mt-10 text-slate-500">
+            <div className="flex justify-center mt-3 text-slate-500">
               <InformationCircleIcon
                 className="w-4 h-4 mr-1"
                 aria-hidden="true"
@@ -64,22 +59,24 @@ export default function Home({ totalUsers }) {
                 Log in with Focusmate
               </Link>
             </div>
+            {totalUsers && (
+              <FadeIn transitionDuration={500}>
+                <p className="mt-5 text-xl font-normal text-center lg:px-10 text-slate-500">
+                  Join{" "}
+                  <span className="underline decoration-orange-400 decoration-wavy">
+                    {totalUsers} Focusmate users
+                  </span>{" "}
+                  and get access to your milestones, session trends, hours of
+                  session, and more.
+                </p>
+              </FadeIn>
+            )}
           </div>
         </div>
       </div>
-      <div className="flex justify-center mt-10 mb-16 sneakpeek">
+      <div className="flex justify-center mt-10 sneakpeek">
         <Image src="/sneakpeek.png" width={1200} height={1200} />
       </div>
-    </FadeIn>
+    </>
   );
-}
-
-export async function getServerSideProps() {
-  const response = await fetch("https://focusbeacon.com/api/stats");
-  const data = await response.json();
-  return {
-    props: {
-      totalUsers: data.totalUsers
-    }
-  };
 }
