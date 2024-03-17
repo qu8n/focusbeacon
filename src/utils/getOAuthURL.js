1ce5980fb4a8a201a1ee1988339ca2377b3be51e
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2024-03-16 22:03:52 -0400

    Update Focusmate OAuth URL and delete sweep.yaml

--- src/utils/getOAuthURL.js
+++ src/utils/getOAuthURL.js
@@ -1,7 +1,7 @@
 // Build the OAuth URL to redirect to Focusmate's OAuth page following their guide:
 // https://focusmate.notion.site/OAuth-Integration-Guide-89dbff2ce67b43fd8719d20a6a7d50c8
 export function getOAuthURL() {
-  const baseURL = "https://www.focusmate.com/oauth/authorize";
+  const baseURL = "https://app.focusmate.com/oauth/authorize";
   const paramsObj = {
     client_id: process.env.NEXT_PUBLIC_FOCUSMATE_CLIENT_ID,
     response_type: "code",
