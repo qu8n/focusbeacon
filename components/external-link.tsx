1c1e97433c52f24ea96c1aff311e9518c923ab9f
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2024-05-29 17:58:57 -0400

    Make external-link open in the same tab by default

--- components/external-link.tsx
+++ components/external-link.tsx
@@ -1,9 +1,10 @@
-export function ExternalLink({ href, children }: {
+export function ExternalLink({ href, openInNewTab = false, children }: {
   href: string;
+  openInNewTab?: boolean;
   children: React.ReactNode;
 }) {
   return (
-    <a href={href} target='_blank' rel='noreferrer'>
+    <a href={href} target={openInNewTab ? "_blank" : "_self"} rel="noopener noreferrer">
       {children}
     </a>
   );
