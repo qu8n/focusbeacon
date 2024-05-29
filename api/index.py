c13146c3d65d8b0183cc44387f4075ecfbfe7fdf
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2024-05-29 17:23:38 -0400

    Enable both FastAPI (/api/py) and NextJS Router Handlers (/api)

--- api/index.py
+++ api/index.py
@@ -7,24 +7,24 @@ app = FastAPI()
 api_key_header = APIKeyHeader(name="CORS-API-Key")
 
 
-async def validate_cors_api_key(cors_api_key: str = Security(api_key_header)):
-    '''This function will be called before /api/secure-data endpoint is called
+async def validate_cors_api_key(incoming_cors_api_key: str = Security(api_key_header)):
+    '''This function will be called before /api/py/secure-data endpoint is called
     to check if the API key is valid. If the API key is not valid, it will
     raise an HTTPException with status code 403.'''
 
-    # cors_api_key = os.getenv("CORS_API_KEY")
-    expected_cors_api_key = "123"
+    actual_cors_api_key = os.getenv("CORS_API_KEY")
 
-    if cors_api_key != expected_cors_api_key:
+    if incoming_cors_api_key != actual_cors_api_key:
         raise HTTPException(status_code=403, detail="Invalid API Key")
-    return cors_api_key
+
+    return incoming_cors_api_key
 
 
-@app.get("/api/secure-data", dependencies=[Depends(validate_cors_api_key)])
+@app.get("/api/py/secure-data", dependencies=[Depends(validate_cors_api_key)])
 async def secure_data():
     return {"message": "You have access to this data"}
 
 
-@app.get("/api/unsecure-data")
+@app.get("/api/py/unsecure-data")
 async def unsecure_data():
     return {"message": "This endpoint is public"}
--- next.config.js
+++ next.config.js
@@ -3,25 +3,11 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api/:path*",
+        source: "/api/py/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
+            ? "http://127.0.0.1:8000/api/pi/:path*"
             : "/api/",
-      },
-      {
-        source: "/docs",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/docs"
-            : "/api/docs",
-      },
-      {
-        source: "/openapi.json",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/openapi.json"
-            : "/api/openapi.json",
       },
     ];
   },
