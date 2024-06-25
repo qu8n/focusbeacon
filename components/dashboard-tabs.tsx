8e823274b2107ef9120baf3a35d6952b25324944
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2024-06-25 08:44:09 -0400

    Add animation effect to dashboard tab navigation

--- components/dashboard-tabs.tsx
+++ components/dashboard-tabs.tsx
@@ -2,29 +2,48 @@
 
 import { Tabs, TabsList, TabsTrigger } from "@/components/tabs"
 import { cx } from "@/lib/utils"
+import { motion } from "framer-motion"
 import Link from "next/link"
 import { useSelectedLayoutSegment } from "next/navigation"
+import { useState } from "react"
 
 const tabNames = ["streak", "weekly", "monthly", "yearly", "lifetime"]
 
 export default function DashboardTabs({ className }: { className?: string }) {
-  const initialTab = useSelectedLayoutSegment()
+  const [activeTab, setActiveTab] = useState(useSelectedLayoutSegment())
 
   return (
     <Tabs
-      defaultValue={initialTab ?? undefined}
+      defaultValue={activeTab ?? undefined}
       className={cx(className, "mt-6 sm:mt-4")}
     >
       <TabsList variant="solid">
-        {tabNames.map((tabName) => {
-          return (
-            <TabsTrigger key={tabName} value={tabName}>
-              <Link className="capitalize" href={`/dashboard/${tabName}`}>
-                {tabName}
-              </Link>
-            </TabsTrigger>
-          )
-        })}
+        {tabNames.map((tabName) => (
+          <TabsTrigger
+            key={tabName}
+            value={tabName}
+            onClick={() => setActiveTab(tabName)}
+            className="relative transition"
+            style={{
+              WebkitTapHighlightColor: "transparent",
+            }}
+          >
+            {activeTab === tabName && (
+              <motion.span
+                layoutId="bubble"
+                className="absolute inset-0 z-10 bg-black rounded-md bg-opacity-20 mix-blend-difference"
+                transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+              />
+            )}
+            <Link
+              prefetch={false}
+              className="capitalize"
+              href={`/dashboard/${tabName}`}
+            >
+              {tabName}
+            </Link>
+          </TabsTrigger>
+        ))}
       </TabsList>
     </Tabs>
   )
