8ee20f811fd2590f0c52fc5fe0ded5270b37dbca
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2024-06-28 18:11:03 -0400

    Fix tab animation

--- components/common/dashboard-tabs.tsx
+++ components/common/dashboard-tabs.tsx
@@ -1,17 +1,16 @@
 "use client"
 
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { cx } from "@/lib/utils"
 import { motion } from "framer-motion"
 import { useRouter, useSelectedLayoutSegment } from "next/navigation"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { LinkInternal } from "@/components/ui/link-internal"
 import { useGetSigninStatus } from "@/hooks/use-get-signin-status"
 
 const tabNames = ["streak", "weekly", "monthly", "yearly", "lifetime"]
 
 export default function DashboardTabs({ className }: { className?: string }) {
-  const [currTab, setCurrTab] = useState(useSelectedLayoutSegment())
+  const currTab = useSelectedLayoutSegment()
 
   const { isSuccess } = useGetSigninStatus()
   const router = useRouter()
@@ -22,40 +21,41 @@ export default function DashboardTabs({ className }: { className?: string }) {
   }, [isSuccess, router])
 
   return (
-    <Tabs
-      defaultValue={currTab ?? undefined}
-      className={cx(className, "mt-6 sm:mt-4")}
+    <div
+      className={cx(
+        className,
+        "mt-6 sm:mt-4 w-fit rounded-md p-2 bg-zinc-200/[0.6] dark:bg-zinc-800"
+      )}
     >
-      <TabsList variant="solid">
-        {tabNames.map((tabName) => (
-          <TabsTrigger
-            key={tabName}
-            value={tabName}
-            onClick={() => {
-              setCurrTab(tabName)
-            }}
-            className="relative transition"
-            style={{
-              WebkitTapHighlightColor: "transparent",
-            }}
+      {tabNames.map((tabName) => (
+        <div
+          key={tabName}
+          className={cx(
+            "relative transition",
+            "inline-flex px-3 py-1 text-sm font-medium",
+            "text-zinc-700 dark:text-zinc-400",
+            "hover:text-zinc-600 hover:dark:text-zinc-300"
+          )}
+          style={{
+            WebkitTapHighlightColor: "transparent",
+          }}
+        >
+          {currTab === tabName && (
+            <motion.span
+              layoutId="bubble"
+              className="absolute inset-0 z-10 bg-black rounded-md bg-opacity-30 mix-blend-difference"
+              transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+            />
+          )}
+          <LinkInternal
+            prefetch={false}
+            className="capitalize"
+            href={`/dashboard/${tabName}`}
           >
-            {currTab === tabName && (
-              <motion.span
-                layoutId="bubble"
-                className="absolute inset-0 z-10 bg-black rounded-md bg-opacity-20 mix-blend-difference"
-                transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
-              />
-            )}
-            <LinkInternal
-              prefetch={false}
-              className="capitalize"
-              href={`/dashboard/${tabName}`}
-            >
-              {tabName}
-            </LinkInternal>
-          </TabsTrigger>
-        ))}
-      </TabsList>
-    </Tabs>
+            {tabName}
+          </LinkInternal>
+        </div>
+      ))}
+    </div>
   )
 }
