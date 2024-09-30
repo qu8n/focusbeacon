23c5fc35c40a4dce5673889662a14729af7dfa47
Brandon Pham <brandonpham13@gmail.com>
2024-09-30 14:29:28 -0500

    consolidated two functions to one

--- api_utils/leaderboard.py
+++ api_utils/leaderboard.py
@@ -1,26 +1,17 @@
 import pandas as pd
 
-def extract_user_streaks(session_counts):
-    '''
-    Converts the pandas dataframe into a list of dictionaries containing data needed for ranking algorithm
-
-    [{user_id: xxx, daily_streak: xxx}, {...}]
-    '''
-
-    columns = ['user_id', 'daily_streak']
-    df = pd.read_csv(session_counts, usecols=columns)
-    list_of_streaks = df.to_dict(orient='records')
-
-    return list_of_streaks
-
-def generate_rank(list_of_streaks):
+def generate_rank(session_counts):
     '''
     Takes a list of dicts containing userIDs and their streaks, and generates a ranking value for each entry based on
     streak value. Returns the list sorted in non-descending order based on ranking.
 
     return: [{user_id: xxx, daily_streak: xxx, rank: xxx}, {...}]
     '''
-    sorted_list_of_streaks = sorted(list_of_streaks, key=lambda x: x['daily_streak'], reverse=True)
+
+    columns = ['user_id', 'daily_streak']   # columns needed from csv
+    df = pd.read_csv(session_counts, usecols=columns)   # extract data from csv to dataframe
+    list_of_streaks = df.to_dict(orient='records')  # convert data fram to list of dicts
+    sorted_list_of_streaks = sorted(list_of_streaks, key=lambda x: x['daily_streak'], reverse=True) # sort by streak
 
     # initialize ranking vars
     rank = 1
@@ -37,8 +28,6 @@ def generate_rank(list_of_streaks):
 
     return sorted_list_of_streaks
 
-# output1 = extract_user_streaks('profile_rows.csv')
-# output2 = generate_rank(output1)
+# output = generate_rank('profile_rows.csv')
 #
-# print(type(output1))
-# print(output2)
+# print(output)
