e09d9a7e012ff5322927f714e571ee2ab84dba56
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2024-10-02 18:03:37 -0400

    Update function to wrap lines following PEP-8 and do minor cleanups

--- api_utils/leaderboard.py
+++ api_utils/leaderboard.py
@@ -1,23 +1,37 @@
 import pandas as pd
 
-def generate_rank(session_counts):
-    '''
-    Takes a list of dicts containing userIDs and their streaks, and generates a ranking value for each entry based on
-    streak value. Returns the list sorted in non-descending order based on ranking.
 
-    return: [{user_id: xxx, daily_streak: xxx, rank: xxx}, {...}]
+def generate_rank(session_counts: str) -> list[dict]:
+    '''
+    Reads a CSV file from the given path containing userIds and their daily
+    streaks, assigns a ranking value to each entry based on the streak value,
+    and handles ties by assigning the same rank to entries with equal streaks.
+
+    Returns:
+        A list of dicts containing userIds, their daily streaks, and their
+        assigned ranks, sorted in descending order based on streak values.
     '''
 
-    columns = ['user_id', 'daily_streak']   # columns needed from csv
-    df = pd.read_csv(session_counts, usecols=columns)   # extract data from csv to dataframe
-    list_of_streaks = df.to_dict(orient='records')  # convert data fram to list of dicts
-    sorted_list_of_streaks = sorted(list_of_streaks, key=lambda x: x['daily_streak'], reverse=True) # sort by streak
+    columns = ['user_id', 'daily_streak']  # columns needed from csv
+
+    df = pd.read_csv(session_counts, usecols=columns)
+
+    # Convert dataframe to list of dicts
+    list_of_streaks = df.to_dict(orient='records')
+
+    # Sort by streak in descending order
+    sorted_list_of_streaks = sorted(
+        list_of_streaks, key=lambda x: x['daily_streak'], reverse=True)
 
     rank = 1
-    skip_rank = 0   # counter for ties, used to determine following rank after a tie
+    skip_rank = 0  # counter for ties, determines following rank after a tie
 
     for i in range(len(sorted_list_of_streaks)):
-        if i == 0 or sorted_list_of_streaks[i]['daily_streak'] != sorted_list_of_streaks[i-1]['daily_streak']:
+        if (
+            i == 0 or
+            sorted_list_of_streaks[i]['daily_streak'] !=
+                sorted_list_of_streaks[i-1]['daily_streak']
+        ):
             rank += skip_rank
             skip_rank = 1
         else:
